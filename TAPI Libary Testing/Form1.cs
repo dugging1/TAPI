232aa148bd0b36b5eb69e68e0819c2f8026ee7a0
dugging1 <goldminergaming@aol.com>
2017-12-19 19:26:20 +0000

    DebugEntry Start.

---
+++ TAPI Libary Testing/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TAPI_Libary_Testing.Properties;
+using TAPI_Library;
+
+namespace TAPI_Libary_Testing {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private DebugEntry de;
+		private void Form1_Load(object sender, EventArgs e) {
+			de = new DebugEntry(this, Resources.Apple, "Apple", "I am an apple.");
+		}
+	}
+}
---
+++ TAPI Library/DebugEntry.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TAPI_Library {
+	public class DebugEntry {
+		Panel master;
+
+		PictureBox pictureBox;
+
+		FlowLayoutPanel textPanel;
+		Label name;
+		Label message;
+
+		public DebugEntry(Control P, Image img, string name, string mess) {
+			master = new Panel() {
+				Parent = P
+			};
+
+			pictureBox = new PictureBox() {
+				Parent=master,
+				Image=img
+			};
+
+			textPanel = new FlowLayoutPanel() {
+				Parent=master,
+				Location=new Point(img.Width, 0)
+			};
+
+			this.name = new Label() {
+				Parent=textPanel,
+				Text=name
+			};
+			message = new Label() {
+				Parent=textPanel,
+				Text=mess
+			};
+			master.Size = new Size(img.Width+textPanel.Width, Math.Max(img.Height, textPanel.Height));
+		}
+	}
+}
