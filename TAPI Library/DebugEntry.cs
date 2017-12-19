75a39d61b83d586106cf9196e806e8d17a871677
dugging1 <goldminergaming@aol.com>
2017-12-19 21:24:25 +0000

    DebugEntry.

--- TAPI Library/DebugEntry.cs
+++ TAPI Library/DebugEntry.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using TAPI_Library.Properties;
 
 namespace TAPI_Library {
 	public enum DebugEntryType { INFO, WARNING, ERROR, MESSAGE }
