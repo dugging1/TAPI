0da8ad2710d46b45fe4d83c6062f1659da9d7df9
dugging1 <goldminergaming@aol.com>
2017-12-19 20:12:58 +0000

    DebugEntry.

--- TAPI Library/DebugEntry.cs
+++ TAPI Library/DebugEntry.cs
@@ -5,42 +5,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TAPI_Library.Properties;
 
 namespace TAPI_Library {
 	public class DebugEntry {
 		Panel master;
 
-		PictureBox pictureBox;
+		PictureBox icon;
 
 		FlowLayoutPanel textPanel;
+
+		Label nameLabel;
 		Label name;
-		Label message;
+		Label messageLabel;
+		RichTextBox message;
 
 		public DebugEntry(Control P, Image img, string name, string mess) {
 			master = new Panel() {
-				Parent = P
+				Parent = P,
+				AutoSize = true,
+				AutoSizeMode = AutoSizeMode.GrowAndShrink,
+				MinimumSize = new Size(256, 128),
+				Size = new Size(128, 64),
+				BorderStyle = BorderStyle.Fixed3D
 			};
 
-			pictureBox = new PictureBox() {
+			icon = new PictureBox() {
 				Parent=master,
 				Image=img,
-				Size=img.Size
+				Size=img.Size,
+				Location=new Point(10,10),
 			};
 
 			textPanel = new FlowLayoutPanel() {
 				Parent=master,
-				Location=new Point(img.Width, 0)
+				Location=new Point(img.Width+10, 0+10),
+				BorderStyle= BorderStyle.Fixed3D
 			};
 
+			nameLabel = new Label() {
+				Parent=textPanel,
+				Text="Name: "
+			};
 			this.name = new Label() {
 				Parent=textPanel,
-				Text=name
+				Text=name,
+				BorderStyle= BorderStyle.FixedSingle,
+				Enabled=false
 			};
-			message = new Label() {
+			messageLabel = new Label() {
+				Parent = textPanel,
+				Text="Message: "
+			};
+			message = new RichTextBox() {
 				Parent=textPanel,
-				Text=mess
+				Text=mess,
+				Multiline=true,
+				Enabled=false
 			};
-			master.Size = new Size(img.Width+textPanel.Width, Math.Max(img.Height, textPanel.Height));
+			//master.Size = new Size(img.Width+textPanel.Width+2*10, Math.Max(img.Height, textPanel.Height)+2*10);
 		}
 	}
 }
