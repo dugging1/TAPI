b0301f2a784f697378cfdd071bbc47f30c945045
dugging1 <goldminergaming@aol.com>
2017-12-19 18:59:42 +0000

    Initial Commit

---
+++ FontTesting/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace FontTesting {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.Log = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// Log
+			// 
+			this.Log.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.Log.Location = new System.Drawing.Point(0, 0);
+			this.Log.Name = "Log";
+			this.Log.Size = new System.Drawing.Size(284, 261);
+			this.Log.TabIndex = 0;
+			this.Log.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.Log);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox Log;
+	}
+}
+
---
+++ FontTesting/Form1.cs
@@ -0,0 +1,28 @@
+﻿using FontTesting.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FontTesting {
+	public partial class Form1 : Form {
+		private static PrivateFontCollection fonts = new PrivateFontCollection();
+		private static Font F;
+
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			fonts.AddFontFile("Fonts\\UnDotum.ttf");
+			F = new Font(fonts.Families[0].Name, 16, FontStyle.Regular, GraphicsUnit.Pixel);
+			Log.Font = F;
+		}
+	}
+}
